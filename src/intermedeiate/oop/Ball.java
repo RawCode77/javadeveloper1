445c660ab479755cca9f1d2c26df39c3e99884fa
Rudy Washington <rawfitgroup@gmail.com>
2024-05-07 01:15:13 -0400

    my initial commit

---
+++ src/intermedeiate/oop/Ball.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intermedeiate.oop;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Ball {
+
+    public String color;
+    public String name;
+    public int capacity;
+    public int bounceRate;
+
+    public void bounce() {
+    System.out.println("Bouncing...");
+
+    }
+
+    public void deflate() {
+    System.out.println("Deflating....");
+
+    }
+
+    public void inflates() {
+    System.out.println("Inflating...");
+
+    }
+
+}
